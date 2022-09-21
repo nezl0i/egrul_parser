f4a113c7e15f8eed768fed559cc86a6cf527e078
nezl0i <nezl0in@gmail.com>
2022-09-21 10:10:34 +0300

    Refactoring. Added function.

--- ogrn_csv.py
+++ ogrn_csv.py
@@ -11,66 +11,81 @@ url_2 = 'https://egrul.nalog.ru/vyp-download/'  # URL для выгрузки pd
 
 inn_file = 'inn.txt'  # Файл со списком ИНН
 invalid_file = 'invalid.txt'  # Файл для не валидных ИНН
-consumers_file = 'consumers.csv'  # Файл с результатом парсинга
+consumers_file = 'consumers_2.csv'  # Файл с результатом парсинга
+
+fieldnames = ['Наименование', 'Адрес регистрации', 'ИНН', 'КПП',
+              'ОГРН/ОГРНИП', 'Дата присвоения ОГРН', 'Директор']
+
+count = 1
 
 s = requests.Session()
 s.get(url + '/index.html')
 
 
+# Запись в файл не валидных ИНН
 def set_invalid(invalid_inn):
     with open(invalid_file, "a", encoding='utf-8') as f:
         f.write(f'{invalid_inn}\n')
 
 
-# Файл со списком ИНН
+# Основной запрос к сайту с установкой cookie (мы же честные ;) )
+def set_cookie(valid_inn):
+    r = s.post(url, data={'query': valid_inn}, cookies=s.cookies)
+    return s.get(url_1 + r.json()['t'], cookies=s.cookies)
+
+
+# Запись в файл информации по валидным ИНН
+def write_valid(result_info: dict):
+    with open(consumers_file, 'a', encoding='utf8', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+        writer.writeheader()  # Заголовок csv файла. Для первого запуска скрипта, раскомментировать.
+        writer.writerow(result_info)
+
+
+# Формируем список ИНН
 with open(inn_file, encoding='utf8') as file:
     inn_list = file.readlines()
 
-with open(consumers_file, 'a', newline='') as csvfile:
-    fieldnames = ['Наименование', 'Адрес регистрации', 'ИНН', 'КПП',
-                  'ОГРН/ОГРНИП', 'Дата присвоения ОГРН', 'Директор']
+# Основной цикл поиска данных по ИНН
+for inn in inn_list:
 
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
-    # writer.writeheader()  # Заголовок csv файла. Для первого запуска скрипта, раскомментировать.
-    count = 1
+    inn = inn.strip()
 
-    # Основной цикл поиска данных по ИНН
-    for inn in inn_list:
+    try:
+        r1 = set_cookie(inn)
+    except KeyError:
+        sleep(5)
+        print('Sleeping 5 sec ...')
+        r1 = set_cookie(inn)
 
-        inn = inn.strip()
-        r = s.post(url, data={'query': inn}, cookies=s.cookies)
+    try:
+        rows = r1.json()['rows'][0]
+        result = {
+            fieldnames[0]: rows.get('n'),
+            fieldnames[1]: rows.get('a'),
+            fieldnames[2]: rows.get('i'),
+            fieldnames[3]: rows.get('p'),
+            fieldnames[4]: rows.get('o'),
+            fieldnames[5]: rows.get('r'),
+            fieldnames[6]: rows.get('g')
+        }
 
-        '''Здесь можно нарваться на KeyError. Вероятно ошибка на стороне
-        сервера. Обрабатывать исключение не нужно, просто повторно запустить
-        скрипт.
-        '''
-        r1 = s.get(url_1 + r.json()['t'], cookies=s.cookies)
+        values = set(result.values())
 
-        try:
-            rows = r1.json()['rows'][0]
-            result = {
-                fieldnames[0]: rows.get('n'),
-                fieldnames[1]: rows.get('a'),
-                fieldnames[2]: rows.get('i'),
-                fieldnames[3]: rows.get('p'),
-                fieldnames[4]: rows.get('o'),
-                fieldnames[5]: rows.get('r'),
-                fieldnames[6]: rows.get('g')
-            }
-
-            values = set(result.values())
-
-            if len(values) > 1:
-                writer.writerow(result)
-            else:
-                set_invalid(inn)
-
+        if len(values) > 1:
+            write_valid(result)
             print(f'{count}. {inn} - Done.')
-            count += 1
-            sleep(2)  # Таймаут между запросами
-
-        except (IndexError, KeyError):
-            # Невалидные ИНН пишем в файл
+        else:
             set_invalid(inn)
+            print(f'{count}. {inn} - Invalid.')
+
+        count += 1
+
+        # Таймаут между запросами
+        sleep(2)
+
+    except (IndexError, KeyError):
+        # Не валидные ИНН пишем в файл
+        set_invalid(inn)
 
 print('Write .csv file is done.')
