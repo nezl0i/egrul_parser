37d92cdecac581b5a67b5bac9c52fd2ce2647a09
nezl0i <nezl0in@gmail.com>
2022-09-21 07:12:33 +0300

    Update comment

--- ogrn_csv.py
+++ ogrn_csv.py
@@ -1,22 +1,25 @@
+# Парсер информации о контрагентах по ИНН с сайта https://egrul.nalog.ru
+# https://github.com/nezl0i/egrul_parser
+
 import csv
-from time import sleep
 import requests
+from time import sleep
 
 url = 'https://egrul.nalog.ru'  # Основной сайт
-url_1 = 'https://egrul.nalog.ru/search-result/'     # URL для поиска по ИНН
-url_2 = 'https://egrul.nalog.ru/vyp-download/'     # URL для выгрузки pdf (не используется)
+url_1 = 'https://egrul.nalog.ru/search-result/'  # URL для поиска по ИНН
+url_2 = 'https://egrul.nalog.ru/vyp-download/'  # URL для выгрузки pdf (не используется)
 
-inn_file = 'inn.txt'    # Файл со списком ИНН
-invalid_file = 'invalid.txt'    # Файл для не валидных ИНН
-consumers_file = 'consumers.csv'    # Файл с результатом парсинга
+inn_file = 'inn.txt'  # Файл со списком ИНН
+invalid_file = 'invalid.txt'  # Файл для не валидных ИНН
+consumers_file = 'consumers.csv'  # Файл с результатом парсинга
 
 s = requests.Session()
 s.get(url + '/index.html')
 
 
 def set_invalid(invalid_inn):
-    with open(invalid_file, "a", encoding='utf-8') as f_:
-        f_.write(f'{invalid_inn}\n')
+    with open(invalid_file, "a", encoding='utf-8') as f:
+        f.write(f'{invalid_inn}\n')
 
 
 # Файл со списком ИНН
@@ -45,14 +48,15 @@ with open(consumers_file, 'a', newline='') as csvfile:
 
         try:
             rows = r1.json()['rows'][0]
-            result = {'Наименование': rows.get('n'),
-                      'Адрес регистрации': rows.get('a'),
-                      'ИНН': rows.get('i'),
-                      'КПП': rows.get('p'),
-                      'ОГРН/ОГРНИП': rows.get('o'),
-                      'Дата присвоения ОГРН': rows.get('r'),
-                      'Директор': rows.get('g')
-                      }
+            result = {
+                fieldnames[0]: rows.get('n'),
+                fieldnames[1]: rows.get('a'),
+                fieldnames[2]: rows.get('i'),
+                fieldnames[3]: rows.get('p'),
+                fieldnames[4]: rows.get('o'),
+                fieldnames[5]: rows.get('r'),
+                fieldnames[6]: rows.get('g')
+            }
 
             values = set(result.values())
 
@@ -63,7 +67,7 @@ with open(consumers_file, 'a', newline='') as csvfile:
 
             print(f'{count}. {inn} - Done.')
             count += 1
-            sleep(2)    # Таймаут между запросами
+            sleep(2)  # Таймаут между запросами
 
         except (IndexError, KeyError):
             # Невалидные ИНН пишем в файл
